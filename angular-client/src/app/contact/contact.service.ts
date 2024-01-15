9aa2f3b0682e365ea819b2b7001554a00a313712
Rajat Verma <cserajatverma@gmail.com>
2024-01-15 21:23:31 +0530

    Update contact.service.ts

--- angular-client/src/app/contact/contact.service.ts
+++ angular-client/src/app/contact/contact.service.ts
@@ -12,6 +12,6 @@ export class ContactService {
 
   addUser(user: any): Observable<any> {
     console.log('USERDATA into service:::POST', user);
-    return this.httpClient.post(`${this.apiUrl}/saveUserInfo`, user);
+    return this.httpClient.post(`${this.apiUrl}/saveUserContactInfo`, user);
   }
 }
