8e1e82744dbe982357ea06f2853d47470bad4025
Rajat Verma <cserajatverma@gmail.com>
2024-01-15 21:24:56 +0530

    Update contact.component.ts

--- angular-client/src/app/contact/contact.component.ts
+++ angular-client/src/app/contact/contact.component.ts
@@ -26,4 +26,44 @@ export class ContactComponent {
     private contactService: ContactService,
     private fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.userForm = this.fb.group({
+      _id: '',
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+          ),
+        ],
+      ],
+      mobileNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/
+          ),
+          // Validators.maxLength(13),
+        ],
+      ],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      address: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(\d{1,}) [a-zA-Z0-9\s]+(\,)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}$/
+          ),
+        ],
+      ],
+    });
+  }
 }
