1e2bbdb4e5e66b1baa91c1cc85dba3d377576081
Rajat Verma <cserajatverma@gmail.com>
2024-01-15 21:21:08 +0530

    Update contact.component.ts

--- angular-client/src/app/contact/contact.component.ts
+++ angular-client/src/app/contact/contact.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
   submitted = false;
 
   email: any;
 
   userForm: any = new FormGroup({});
 
-  constructor(private contactService: ContactService, private fb: FormBuilder) {}
+  constructor(
+    private contactService: ContactService,
+    private fb: FormBuilder
+  ) {}
 }
