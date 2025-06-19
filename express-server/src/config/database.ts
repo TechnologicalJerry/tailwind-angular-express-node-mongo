4f7475c34a584db2f6383d5f7328ff8b77ff8d2c
TechnologicalJerry <cserajatverma@gmail.com>
2025-06-20 00:49:12 +0530

    Create database.ts

---
+++ express-server/src/config/database.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const mongoURI = process.env.LOCAL_DATABASE_URL || "mongodb://localhost:27017/test-db";
+        await mongoose.connect(mongoURI);
+        console.log("MongoDB connected");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
