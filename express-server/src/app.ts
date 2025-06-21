3327c394538d56460b78ebca3ad58b9ee0090749
TechnologicalJerry <cserajatverma@gmail.com>
2025-06-21 10:06:05 +0530

    Update app.ts

--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -3,10 +3,16 @@ import bodyParser from "body-parser";
 import userRoutes from "./routes/user.routes";
 import productRoutes from "./routes/product.routes";
 import connectDB from "./config/database";
+import cors from "cors";
 
 const app = express();
 
 connectDB();
+
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+}));
 
 app.use(bodyParser.json());
 app.use("/api/users", userRoutes);
