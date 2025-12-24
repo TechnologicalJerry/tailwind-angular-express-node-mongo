31ec0916ad9480cc8ac9b033ee3a44491bd7c90b
Rajat Verma <cserajatverma@gmail.com>
2025-12-24 19:46:23 +0530

    Add main application routes
    Defined routes for home, login, signup, about, contact, and a wildcard not-found page. Also added a redirect from 'home' to the root path.

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./features/auth/signup/signup').then((m) => m.Signup),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about').then((m) => m.About),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact').then((m) => m.Contact),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/components/not-found/not-found').then((m) => m.NotFound),
+  },
+];
