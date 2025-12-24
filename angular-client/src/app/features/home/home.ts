c80ba416eb9e25e25558072073355257b7f12c71
Rajat Verma <cserajatverma@gmail.com>
2025-12-24 21:50:22 +0530

    Enable OnPush change detection in Home component
    Added ChangeDetectionStrategy.OnPush to the Home component for improved performance. Also imported RouterLink and cleaned up the class definition.

--- angular-client/src/app/features/home/home.ts
+++ angular-client/src/app/features/home/home.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {
-
-}
+export class Home {}
